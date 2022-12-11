dfd7faf56c07fc363cafc5d6f65e5e189a86546f
dimas360 <st-92dimon@mail.ru>
2022-12-11 12:06:58 +0300

    task_v1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+
+string[] array_in = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array_out = new string[array_in.Length];
+void SecondArrayWithIF(string[] array_in, string[] array_out)
+{
+    int count = 0;
+    for (int i = 0; i < array_in.Length; i++)
+    {
+    if(array_in[i].Length <= 3)
+        {
+        array_out[count] = array_in[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array_in, array_out);
+PrintArray(array_out);
